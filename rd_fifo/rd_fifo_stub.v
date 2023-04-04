678d9b55859c9f8688cabacfe05c4ad793a51f42
GDUT_CJJ <103297071+gdutcjj@users.noreply.github.com>
2023-04-04 10:14:30 +0800

    Add files via upload

---
+++ rd_fifo/rd_fifo_stub.v
@@ -0,0 +1,33 @@
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
+// Date        : Mon Feb 20 16:35:38 2023
+// Host        : DESKTOP-9F2704S running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               e:/Verilog/CAM-HDMI-test2-0220/CAM-HDMI-test2-0220.srcs/sources_1/ip/rd_fifo/rd_fifo_stub.v
+// Design      : rd_fifo
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7k325tffg676-2
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "fifo_generator_v13_2_5,Vivado 2019.2" *)
+module rd_fifo(rst, wr_clk, rd_clk, din, wr_en, rd_en, dout, full, 
+  empty, rd_data_count, wr_data_count, wr_rst_busy, rd_rst_busy)
+/* synthesis syn_black_box black_box_pad_pin="rst,wr_clk,rd_clk,din[255:0],wr_en,rd_en,dout[255:0],full,empty,rd_data_count[8:0],wr_data_count[8:0],wr_rst_busy,rd_rst_busy" */;
+  input rst;
+  input wr_clk;
+  input rd_clk;
+  input [255:0]din;
+  input wr_en;
+  input rd_en;
+  output [255:0]dout;
+  output full;
+  output empty;
+  output [8:0]rd_data_count;
+  output [8:0]wr_data_count;
+  output wr_rst_busy;
+  output rd_rst_busy;
+endmodule
